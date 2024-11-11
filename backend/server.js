95048fd9597eafd8154ed4722aab854949e36eb9
Abdul.Alhazemi <abdulmajeedalhazemi@gmail.com>
2024-11-11 20:57:05 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,43 @@
 import express from "express";
 import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import cors from "cors";
 import dotenv from "dotenv";
-import AuthRoute from './Routes/AuthRoute.js'
-import UserRoute from './Routes/UserRoute.js'
-import PostRoute from './Routes/PostRoute.js'
+import mongoose from "mongoose";
+
+
+// routes
+import AuthRoute from './routes/AuthRoute.js'
+import UserRoute from './routes/UserRoute.js'
+import PostRoute from './routes/PostRoute.js'
+import UploadRoute from './Routes/uploadRoute.js'
+import ChatRoute from './Routes/chatRoute.js'
+import MessageRoute from './Routes/massageRoute.js'
 
 const app = express();
 
+
+// middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+// to serve images inside public folder
+app.use(express.static('public'));
+app.use('/images', express.static('images'));
+
 
 dotenv.config();
+const PORT = process.env.PORT;
 
+const CONNECTION = process.env.MONGODB_CONNECTION;
 mongoose
-  .connect(process.env.MONGO_DB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() =>
-    app.listen(process.env.PORT, () =>
-      console.log(`Listening at ${process.env.PORT}`)
-    )
-  )
-  .catch((error) => console.log(error));
+  .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-app.use('/auth', AuthRoute)
+
+app.use('/auth', AuthRoute);
 app.use('/user', UserRoute)
-app.use('/post', PostRoute)
+app.use('/posts', PostRoute)
+app.use('/upload', UploadRoute)
+app.use('/chat', ChatRoute)
+app.use('/message', MessageRoute)
